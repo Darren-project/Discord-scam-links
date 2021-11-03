56b596a86e5c80d76870dc82c8208e0772487fa6
mickeydarrenlau <darrenwjlau@gmail.com>
2021-11-03 09:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ f = open("nitroscamlinks.txt", "r")
 for f1 in f:
  for line in f1.split():
     helper.append(line)
+helper = set(helper)
+helper2 = []
+for i in helper:
+    helper2.append(i.replace('https://',''))
+helper = helper2
 df = helper
 f.close()
 print(df)
